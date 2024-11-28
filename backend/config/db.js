60ccf9189bd537d88de9928e0fdd0ee3554038e9
sudheer <68139555+sudheer416@users.noreply.github.com>
2024-11-29 00:47:05 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
             // useNewUrlParser: true,
             // useUnifiedTopology: true,
         // });
-            )
+            
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
