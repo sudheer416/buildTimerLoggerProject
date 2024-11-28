bf937a3e0f5af8b251b7b9c25ec7a346bfc71ae9
sudheer <68139555+sudheer416@users.noreply.github.com>
2024-11-29 01:02:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const connectDB = require('./config/db');
 const timerRoutes = require('./routes/timerRoutes');
 require('dotenv').config();
 const errorHandler = require("./middleware/errorHandler")
-
+const AppError =require("./utils/AppError")
 const app = express();
 
 app.use(cors(
@@ -21,9 +21,13 @@ connectDB();
 
 app.use('/', timerRoutes);
 
-app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+app.all("*",(req,res,next)=>{
+    next(new AppError(`can't find ${req.originalUrl} on this server`))
+})
+
+app.use(errorHandler);
